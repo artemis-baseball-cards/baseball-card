322e575fbcd6dd949559bef09cf299bff168a0fd
mo <meganannetteolson@yahoo.com>
2018-01-22 15:26:44 -0800

    Add basic CRUD to employee controller.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,73 +1,50 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
 
-  # GET /employees
-  # GET /employees.json
   def index
     @employees = Employee.all
   end
 
-  # GET /employees/1
-  # GET /employees/1.json
   def show
+    @employee = Employee.find(params[:id])
   end
 
-  # GET /employees/new
   def new
     @employee = Employee.new
   end
 
-  # GET /employees/1/edit
-  def edit
-  end
-
-  # POST /employees
-  # POST /employees.json
   def create
     @employee = Employee.new(employee_params)
-
-    respond_to do |format|
-      if @employee.save
-        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
-        format.json { render :show, status: :created, location: @employee }
-      else
-        format.html { render :new }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
-      end
+    if @employee.save
+      flash[:notice] = "Your Employee has been saved!"
+      redirect_to employees_path
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /employees/1
-  # PATCH/PUT /employees/1.json
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
   def update
-    respond_to do |format|
-      if @employee.update(employee_params)
-        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
-        format.json { render :show, status: :ok, location: @employee }
-      else
-        format.html { render :edit }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
-      end
+    @employee = Employee.find(params[:id])
+    if @employee.update(employee_params)
+      flash[:notice] = "Your Employee has been updated!"
+      redirect_to employees_path
+    else
+      render :edit
     end
   end
 
-  # DELETE /employees/1
-  # DELETE /employees/1.json
   def destroy
+    @employee = Employee.find(params[:id])
     @employee.destroy
-    respond_to do |format|
-      format.html { redirect_to employees_url, notice: 'Employee was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to employees_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_employee
-      @employee = Employee.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def employee_params
       params.require(:employee).permit(:name, :position, :start_date, :personal_development_plan, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal, :area_to_develop, :objective_setting, :personal_goal, :project_id, :team_id)
     end
