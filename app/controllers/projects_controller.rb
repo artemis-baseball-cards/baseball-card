e3138880debc3f181e269454c9e50f4c236032f2
mo <meganannetteolson@yahoo.com>
2018-01-22 15:56:41 -0800

    Add basic crud for teams and projects to be created initially.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,74 +1,55 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  # before_action :set_project, only: [:show, :edit, :update, :destroy]
 
-  # GET /projects
-  # GET /projects.json
   def index
-    @projects = Project.all
+    projects = Project.all
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
   def show
+    @project = Project.find(params[:id])
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
-  def edit
-  end
-
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(project_params)
-
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.save
+      flash[:notice] = "Your Project has been saved!"
+      redirect_to projects_path
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
+  def edit
+    @project = Project.find(params[:id])
+  end
+
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    @project = Project.find(params[:id])
+    if @project.update(employee_params)
+      flash[:notice] = "Your project has been updated!"
+      redirect_to projects_path
+    else
+      render :edit
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
+    @project = Project.find(params[:id])
     @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to projects_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def project_params
-      params.require(:project).permit(:title, :start_date, :end_date, :project_type, :role, :key_skill)
-    end
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :start_date, :end_date, :project_type, :role, :key_skill)
+  end
 end
