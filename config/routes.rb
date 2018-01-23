e3854e94d83bf7cd7c045ae6fda01e9cec39fc6b
Gennaro <g.anzalone12.com>
2018-01-23 11:07:23 -0800

    remove team resources from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :others
-  resources :teams
   resources :projects
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
