81016037802c372ae81d655678143aecd978781f
Gennaro <g.anzalone12.com>
2018-01-23 11:06:31 -0800

    update schema with dropped teams table and projects table with additional columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122233125) do
+ActiveRecord::Schema.define(version: 20180123185739) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
@@ -53,14 +53,8 @@ ActiveRecord::Schema.define(version: 20180122233125) do
     t.string "key_skill"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "teams", force: :cascade do |t|
     t.text "objective"
-    t.string "start_date"
     t.text "goals"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
