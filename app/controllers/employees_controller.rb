4f71116d2ea028f51c7f8d56ac86f094198f8dfe
mo <meganannetteolson@yahoo.com>
2018-01-23 13:59:52 -0800

    Fix small bugs.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -49,7 +49,7 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id])
   end
 
-    def employee_params
-      params.require(:employee).permit(:name, :position, :start_date, :personal_development_plan, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal, :area_to_develop, :objective_setting, :personal_goal, :project_id, :team_id)
-    end
+  def employee_params
+    params.require(:employee).permit(:name, :position, :start_date, :personal_development_plan, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal, :area_to_develop, :objective_setting, :personal_goal, :project_id, :team_id)
+  end
 end
