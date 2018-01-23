4c6524bfe7b4ba66713f7fd139ea1e96891843d0
mo <meganannetteolson@yahoo.com>
2018-01-23 12:27:42 -0800

    Fix variable name and set_employee method.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -45,6 +45,10 @@ class EmployeesController < ApplicationController
 
   private
 
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
     def employee_params
       params.require(:employee).permit(:name, :position, :start_date, :personal_development_plan, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal, :area_to_develop, :objective_setting, :personal_goal, :project_id, :team_id)
     end
