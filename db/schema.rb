6963fd69d0b38fa382c2f11a3cf5cf722815cd52
Gennaro <g.anzalone12.com>
2018-01-24 13:45:41 -0800

    update the schema to reflect the changes made in the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124204205) do
+ActiveRecord::Schema.define(version: 20180124214310) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
     t.string "position"
-    t.string "start_date"
+    t.date "start_date"
     t.text "personal_development_plan"
     t.string "life_quote"
     t.string "quote"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 20180124204205) do
     t.string "area_to_develop"
     t.string "objective_setting"
     t.string "personal_goal"
-    t.integer "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -50,8 +49,8 @@ ActiveRecord::Schema.define(version: 20180124204205) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
-    t.string "start_date"
-    t.string "end_date"
+    t.date "start_date"
+    t.date "end_date"
     t.string "project_type"
     t.string "role"
     t.string "key_skill"
