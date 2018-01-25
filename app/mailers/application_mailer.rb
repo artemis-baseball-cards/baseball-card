7393cef7e39b93e0dba13feabb67a4d7e5ffadb4
erikzaksf <erik-zak-sf@gmail.com>
2018-01-25 12:44:32 -0800

    finish setting up mailer for testing

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: 'artemis.test@yandex.com'
+
+  def send_welcome_email(user)
+    @user = user
+    mail(:to => @user.email, :subject => "Welcome!")
+  end
 end
