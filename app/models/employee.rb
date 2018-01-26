ee1841ee8828cca2b39a3e333932dbdf14060ce7
Gennaro <g.anzalone12.com>
2018-01-26 10:20:53 -0800

    add scrolling methods to employee models and views

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,13 @@
 class Employee < ApplicationRecord
   has_and_belongs_to_many :projects
+
+
+  def self.previous_employee
+    self.class.where("name < ?", name).order(name: :desc).first
+  end
+
+  def self.next_employee
+    self.class.where("name < ?", name).order(name: :asc).first
+  end
+
 end
