3065cce8f9051f59093f7863ce154bbedc937e38
erikzaksf <erik-zak-sf@gmail.com>
2018-01-29 15:26:10 -0800

    add employee user relation in rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129200505) do
+ActiveRecord::Schema.define(version: 20180129231259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180129200505) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "start_date"
+    t.integer "user_id"
   end
 
   create_table "employees_projects", force: :cascade do |t|
