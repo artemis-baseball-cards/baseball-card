54b3c30cf7e1963496aa630f66b934d55f6765d6
mo <meganannetteolson@yahoo.com>
2018-01-31 16:02:05 -0800

    Update others controller.

--- app/controllers/others_controller.rb
+++ app/controllers/others_controller.rb
@@ -1,73 +1,58 @@
 class OthersController < ApplicationController
   before_action :set_other, only: [:show, :edit, :update, :destroy]
 
-  # GET /others
-  # GET /others.json
   def index
     @others = Other.all
   end
 
-  # GET /others/1
-  # GET /others/1.json
   def show
+    @other = Other.find(params[:id])
   end
 
-  # GET /others/new
   def new
+    @employee = Employee.find(params[:employee_id])
     @other = Other.new
   end
 
-  # GET /others/1/edit
   def edit
+    @employee = Employee.find(params[:employee_id])
+    @other = Other.find(params[:id])
   end
 
-  # POST /others
-  # POST /others.json
   def create
-    @other = Other.new(other_params)
-
-    respond_to do |format|
-      if @other.save
-        format.html { redirect_to @other, notice: 'Other was successfully created.' }
-        format.json { render :show, status: :created, location: @other }
-      else
-        format.html { render :new }
-        format.json { render json: @other.errors, status: :unprocessable_entity }
-      end
+    @employee = Employee.find(params[:employee_id])
+    @other = @employee.others.new(other_params)
+    if @other.update(other_params)
+      flash[:notice] = "Your input has been saved!"
+      redirect_to employee_path(@employee)
+    else
+      render :edit
     end
   end
 
-  # PATCH/PUT /others/1
-  # PATCH/PUT /others/1.json
   def update
-    respond_to do |format|
-      if @other.update(other_params)
-        format.html { redirect_to @other, notice: 'Other was successfully updated.' }
-        format.json { render :show, status: :ok, location: @other }
-      else
-        format.html { render :edit }
-        format.json { render json: @other.errors, status: :unprocessable_entity }
-      end
+    @employee = Employee.find(params[:employee_id])
+    @other = Other.find(params[:id])
+    if @other.update(other_params)
+      flash[:notice] = "Your input has been updated!"
+      redirect_to employee_path(@employee)
+    else
+      render :edit
     end
   end
 
-  # DELETE /others/1
-  # DELETE /others/1.json
+
   def destroy
+    @other = Other.find(params[:id])
     @other.destroy
-    respond_to do |format|
-      format.html { redirect_to others_url, notice: 'Other was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to employees_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_other
       @other = Other.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def other_params
       params.require(:other).permit(:occasion, :end_project, :birthday, :pulse_survey)
     end
