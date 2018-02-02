967625f4803c4fa324a63d559e122238f25a3dcd
erikzaksf <erik-zak-sf@gmail.com>
2018-02-02 09:17:39 -0800

    re hide edit button from employee show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201185903) do
+ActiveRecord::Schema.define(version: 20180202012813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180201185903) do
     t.string "willing_to_travel"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "project_roles", array: true
   end
 
   create_table "employees_projects", force: :cascade do |t|
