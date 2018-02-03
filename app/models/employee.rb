9f10728a80ef8178516efa92e41364d2c84767dd
Gennaro <g.anzalone12.com>
2018-02-02 16:06:00 -0800

    add scopes for each skill

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,7 +4,23 @@ Bundler.require
 class Employee < ApplicationRecord
   has_and_belongs_to_many :projects
   has_many :others
-  scope :critical_thinking, -> { where("skills = ? ", ("{Critical thinking}"))}
+
+  scope :flexible, -> { where("skills = ? ", ("{Flexible}"))}
+  scope :team_player, -> { where("skills = ? ", ("{Team Player}"))}
+  scope :adaptable, -> { where("skills = ? ", ("{Adaptable}"))}
+  scope :accountable, -> { where("skills = ? ", ("{Accountable}"))}
+  scope :problem_solver, -> { where("skills = ? ", ("{Problem Solver}"))}
+  scope :critical_thinker, -> { where("skills = ? ", ("{Critial Thinker}"))}
+  scope :problem_writer, -> { where("skills = ? ", ("{Problem Definition Writer}"))}
+  scope :experimenter, -> { where("skills = ? ", ("{Experimenter}"))}
+  scope :interview_designer, -> { where("skills = ? ", ("{Interview Designer}"))}
+  scope :interview_conductor, -> { where("skills = ? ", ("{Interview Conductor}"))}
+  scope :survey_designer, -> { where("skills = ? ", ("{Survey Designer}"))}
+  scope :isr_synthesizer, -> { where("skills = ? ", ("{Interview/Survey/Research Synthesizer}"))}
+  scope :crm_producer, -> { where("skills = ? ", ("{Client Ready Material Producer}"))}
+  scope :tool_developer, -> { where("skills = ? ", ("{Tool Developer}"))}
+  scope :process_improver, -> { where("skills = ? ", ("{Process Improver}"))}
+  scope :team_leader, -> { where("skills = ? ", ("{Team Leader}"))}
 
   def self.get_data
     session = GoogleDrive::Session.from_service_account_key("client_secret.json")
