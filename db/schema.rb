cf2b054ceccc343be5dffa6cdc7a44e7571747e7
linda luu <linda.luu1226@gmail.com>
2018-02-06 17:29:59 -0800

    add background color to header

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180206201144) do
+ActiveRecord::Schema.define(version: 20180206201320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +93,11 @@ ActiveRecord::Schema.define(version: 20180206201144) do
 
   create_table "skills", force: :cascade do |t|
     t.string "description"
+  end
 
+  create_table "skills_projects", force: :cascade do |t|
+    t.integer "skills_id"
+    t.integer "projects_id"
   end
 
   create_table "users", force: :cascade do |t|
