fccab50ed6b0f2b3dac639c2957ea82a25c7622b
Gennaro <g.anzalone12.com>
2018-02-07 16:07:48 -0800

    add filterrific and scopes for search by name and sorted by name. Begin working on checkbox search scope

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -24,6 +24,58 @@ class Employee < ApplicationRecord
 
   scope :by_industry, -> (industry) { where("industry_experience like ?", "%#{industry}%")}
 
+  filterrific(
+   default_filter_params: { sorted_by: 'created_at_desc' },
+   available_filters: [
+     :sorted_by,
+     :search_query,
+     :with_country_id,
+     :with_created_at_gte,
+     :with_skill,
+     :with_project
+   ]
+ )
+
+scope :search_query, lambda { |query|
+  return nil if query.blank?
+  terms = query.downcase.split(/\s+/)
+  # replace "*" with "%" for wildcard searches,
+  # append '%', remove duplicare '%'s
+  terms = terms.map { |e|
+    (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+  }
+  num_or_conditions = 1
+  where(
+  terms.map {
+    or_clauses = [
+      "LOWER(employees.name) LIKE ?"
+    ].join(' OR ')
+    "(#{ or_clauses })"
+  }.join(' AND '),
+    *terms.map { |e| [e] * num_or_conditions }.flatten
+  )
+}
+
+scope :sorted_by, lambda { |sort_option|
+  direction = (sort_option =~ /desc$/) ? 'desc' : 'asc'
+  case sort_option.to_s
+  when /^created_at_/
+    order("employees.created_at #{ direction }")
+  when /^name_/
+    order("LOWER(employees.name) #{ direction }")
+  else
+    raise(ArgumentError, "Invalid sort option: #{ sort_option.inspect}")
+  end
+}
+
+ scope :with_skill, lambda { |skill_ids|
+  where(:skill_id => [*skill_ids])
+}
+
+scope :with_project, lambda { |project_ids|
+ where(:project_id => [*project_ids])
+}
+
   def self.get_data
     session = GoogleDrive::Session.from_service_account_key("client_secret.json")
     spreadsheet = session.spreadsheet_by_title("360 Feedback Form (Responses)")
@@ -37,5 +89,7 @@ class Employee < ApplicationRecord
     worksheet = spreadsheet.worksheets.first
     area_data = worksheet.rows.each { |row| puts row.join(" | ") }
   end
+
+
 
 end
