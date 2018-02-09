00814c459953e495b8d72d9ffda48e1197ce2fc5
L.D.MacKrell <ldmackrell@gmail.com>
2018-02-09 11:38:31 -0800

    Update employee and projects controllers with roles

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -10,6 +10,7 @@ class EmployeesController < ApplicationController
     @id = @employee.id
     @projects = Project.all
     @skills = Skill.all
+    @roles = Role.all
   end
 
   def flip
@@ -45,6 +46,7 @@ class EmployeesController < ApplicationController
   def new
     @projects = Project.all
     @skills = Skill.all
+    @role = Role.all
     @employee = Employee.new
     @atd_data = Employee.get_atd
   end
@@ -64,6 +66,7 @@ class EmployeesController < ApplicationController
   def edit
     @projects = Project.all
     @skills = Skill.all
+    @role = Role.all
     @employee = Employee.find(params[:id])
   end
 
@@ -97,7 +100,7 @@ class EmployeesController < ApplicationController
   end
 
   def employee_params
-    params.require(:employee).permit(:name, :position, :start_date, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal,  :objective_setting, :personal_goal, :mission, :expectation, :awesome, :need, :receive_feedback, :development, :personal_goal_two, :personal_goal_three, :personal_goal_four, :country, :state, :city, :willing_to_travel, :areas_to_develop, :remote, :industry_experience => [], :project_ids => [], :skill_ids => [], :project_roles => [])
+    params.require(:employee).permit(:name, :position, :start_date, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal,  :objective_setting, :personal_goal, :mission, :expectation, :awesome, :need, :receive_feedback, :development, :personal_goal_two, :personal_goal_three, :personal_goal_four, :country, :state, :city, :willing_to_travel, :areas_to_develop, :remote, :industry_experience => [], :project_ids => [], :skill_ids => [], :project_roles => [], :role_ids => [])
   end
 
 end
