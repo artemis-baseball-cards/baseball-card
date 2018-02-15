fad9f7f3dd51ed5468b4c24d5ddd9e4a9230f974
L.D.MacKrell <ldmackrell@gmail.com>
2018-02-15 14:55:49 -0800

    Correct areas to develop in employee controller and other index

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -103,7 +103,7 @@ class EmployeesController < ApplicationController
   end
 
   def employee_params
-    params.require(:employee).permit(:name, :position, :start_date, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal,  :objective_setting, :personal_goal, :mission, :expectation, :awesome, :need, :receive_feedback, :development, :personal_goal_two, :personal_goal_three, :personal_goal_four, :country, :state, :city, :willing_to_travel, :areas_to_develop, :remote, :project_ids => [], :skill_ids => [], :role_ids => [], :industry_ids => [])
+    params.require(:employee).permit(:name, :position, :start_date, :life_quote, :quote, :motto, :strength, :myers_briggs, :birkman, :vision, :short_term_goal, :long_term_goal,  :objective_setting, :personal_goal, :mission, :expectation, :awesome, :need, :receive_feedback, :development, :personal_goal, :personal_goal_two, :personal_goal_three, :country, :state, :city, :willing_to_travel, :areas_to_develop, :remote, :project_ids => [], :skill_ids => [], :role_ids => [], :industry_ids => [])
   end
 
 end
