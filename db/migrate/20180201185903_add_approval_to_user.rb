0afb2545ed17a484e2a3324bea9a0637c124d2ee
erikzaksf <erik-zak-sf@gmail.com>
2018-02-16 09:47:53 -0800

    remove user creation from migration

--- db/migrate/20180201185903_add_approval_to_user.rb
+++ db/migrate/20180201185903_add_approval_to_user.rb
@@ -1,15 +1,8 @@
 class AddApprovalToUser < ActiveRecord::Migration[5.1]
 
   def change
+    
     add_column :users, :approved, :boolean, :default => false, :null => false
-
-    User.create! do |u|
-     u.email     = 'first@first.com'
-     u.password    = 'password'
-     u.password_confirmation = 'password'
-     u.superadmin_role = true
-     u.approved = true
-     end
 
   end
 
